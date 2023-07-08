84a66ca1b15061084323fc50a04a2b3db14ef7a8
Mikhail220220 <135328072+Mikhail220220@users.noreply.github.com>
2023-07-08 21:24:42 +0600

    Add files via upload

---
+++ C#7.3/task1/Program.cs
@@ -0,0 +1,37 @@
+﻿int [,] NewArray ()
+{
+    Console.WriteLine("Enter 1 number:");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter 2 number:");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+     Console.WriteLine("Enter min:");
+    int min = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter max:");
+    int max = Convert.ToInt32(Console.ReadLine());
+
+    int [,] Array = new int [number1, number2];
+
+    for (int i = 0; i < number1; i++ )
+    {
+        for (int j = 0; j < number2; j++)
+        {
+            Array [i,j] = new Random().Next(min, max + 1);
+        }
+    }
+    return Array;
+}
+void PrintArray (int [,] array2d)
+{
+    for (int i = 0; i < array2d.GetLength(0); i++)
+    {
+        for (int j =0; j < array2d.GetLength(1); j++)
+        {
+            Console.Write ( array2d[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int [,] New2dArray = NewArray();
+PrintArray (New2dArray);
